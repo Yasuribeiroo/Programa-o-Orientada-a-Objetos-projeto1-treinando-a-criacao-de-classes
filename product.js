679a6d641bf1608a84000330323b2a5c55d7d0f6
𝘠𝘢𝘴𝘮𝘪𝘮 𝘙𝘪𝘣𝘦𝘪𝘳𝘰 ๋࣭ ⭑ <159492044+Yasuribeiroo@users.noreply.github.com>
2024-08-09 08:27:25 -0300

    Update product.js

--- product.js
+++ product.js
@@ -1,1 +1,39 @@
+class Product {
+    constructor(name, description, price) {
+      this.name = name
+      this.description = description
+      this.price = price
+      this.inStock = 0
+    }
+  
+    addToStock(qtd) {
+      if (qtd > 0) {
+        return this.inStock += qtd
+      }
+    }
 
+  
+    calculateDiscount(porcentagem) {
+        const discount = this.price * (porcentagem / 100)
+        return this.price - discount
+    }
+  
+    console() {
+      console.log(`Nome do produto: ${this.name}`)
+      console.log(`Descrição do produto: ${this.description}`)
+      console.log(`Valor do produto: R$${this.price.toFixed(2)}`)
+      console.log(`Quantidade no estoque: ${this.inStock}`)
+    }
+  }
+  
+  // Exemplo de uso
+  const product = new Product("Notebook", "Notebook Nitro 5", 1000.00)
+  product.addToStock(10)
+  product.console()
+  
+  console.log('==================================');
+  
+  const porcentagem = 5
+  const newPrice = product.calculateDiscount(porcentagem)
+  console.log(`Desconto no produto: ${porcentagem}%`)
+  console.log(`Novo valor do produto: R$${newPrice.toFixed(2)}`)
